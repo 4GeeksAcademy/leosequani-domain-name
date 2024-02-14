557b3d5581fe7e17bbb76c7a7707a4fd1543b31a
leosequani <156928045+leosequani@users.noreply.github.com>
2024-02-14 21:36:06 +0000

    Final Submit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function domainmaker() {
+  let pronoun = ["the", "our", "he", "her"];
+  let adj = ["great", "big", "small", "average"];
+  let noun = ["jogger", "racoon", "car", "pole"];
+  let end = [".com", ".net", ".org", ".edu"];
+  let count = 0;
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < end.length; l++) {
+          count += 1;
+          document.getElementById(
+            "domainstext"
+          ).innerHTML += `<div>${count}. ${pronoun[i]}${adj[j]}${noun[k]}${end[l]}</div>`;
+        }
+      }
+    }
+  }
+  return;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  domainmaker();
 };
